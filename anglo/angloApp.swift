07e3ed0dbd15eec492d49927b3ef6900517a16b2
kaorulego5x <kaorulego5x@gmail.com>
2021-11-27 17:17:56 +0900

    Initial Commit

---
+++ anglo/angloApp.swift
@@ -0,0 +1,17 @@
+//
+//  angloApp.swift
+//  anglo
+//
+//  Created by Kaoru Nishihara on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct angloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
